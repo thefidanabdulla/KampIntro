ac429b7579429c3099dfc7e9c74542feeb075876
thefidanabdulla <thefidanabdulla@gmail.com>
2022-05-13 22:03:14 +0400

    OOP2 added

---
+++ Donguler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazilim gelistirici yetistirme kampi";
+            string kurs2 = "Programlamaya baslangic icin temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+
+            //array
+
+            string[] kurslar = new string[] {
+                "Yazilim gelistirici yetistirme kampi",
+                "Programlamaya baslangic icin temel kurs",
+                "Java",
+                "Python"
+            };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
---
+++ OoP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNo = "12345";
+            customer1.Name = "Fidan";
+            customer1.Surname = "Abdulla";
+            customer1.TcNo = "2222222";
+
+            CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNo = "54321";
+            customer2.CompanyName = "Kodlama.io";
+            customer2.TaxNo = "1234567890";
+
+
+            Customer customer3 = new IndividualCustomer();
+
+            Customer customer4 = new CoorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+
+        }
+    }
+}
