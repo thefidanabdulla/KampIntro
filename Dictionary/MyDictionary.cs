0c4fd1b57dbef7e941f90ac32dfda99ebb0b11ad
thefidanabdulla <thefidanabdulla@gmail.com>
2022-05-13 17:42:52 +0400

    Dictionary project added

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        //Add method
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKeyArray = keys;
+            Value[] tempValueArray = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return keys.Length;  }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Coding");
+            myDictionary.Add(2, "Read");
+            myDictionary.Add(3, "Sport");
+
+            Console.WriteLine(myDictionary.Count);
+
+        }
+    }
+}
